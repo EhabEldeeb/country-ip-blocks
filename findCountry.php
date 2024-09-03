8ad0284a4db3694cb21d6d68be8689ecde6b4c92
Ehab Eldeeb <124740573+EhabEldeeb@users.noreply.github.com>
2024-09-03 16:33:26 +0300

    add API access
    TODO:
    - Authentication (if you want to)
    - Rate Limiting (if you want to)

--- findCountry.php
+++ findCountry.php
@@ -1,13 +1,25 @@
-<form method="POST">
-    <input type="text" name="IP" placeholder="IP" />
-    <button type="submit">Find Country</button>
-</form>
 <?php
-if (isset($_POST['IP'])) {
-    $ip = $_POST['IP'];
+$WebInterface = true;
+if (isset($_GET['api'])) {
+    $WebInterface = false;
+    header('Content-Type: application/json');
+    $response = [];
+} else {
+    ?>
+    <form method="POST">
+        <input type="text" name="IP" placeholder="IP" />
+        <button type="submit">Find Country</button>
+    </form>
+    <?php
+}
+if (isset($_REQUEST['ip'])) {
+    $ip = $_REQUEST['ip'];
     $found = false;
     if (!filter_var($ip, FILTER_VALIDATE_IP)) {
-        echo "Invalid IP Address.";
+        $msg = "Invalid IP Address.";
+        $response = ["status" => "error", "status_code" => 1, "response" => $msg];
+        if ($WebInterface)
+            echo $msg;
         $found = true;
     }
     if (!$found) {
@@ -27,28 +39,58 @@ if (isset($_POST['IP'])) {
             if (count($d) <= 1) {
                 break;
             } else if (($ipAddr > ip2long($localRanges[0]) && $ipAddr <= ip2long($localRanges[1]))) {
-                echo "This is a local IP. it only exists in a private network (Class A).";
+                $msg = "This is a local IP. it only exists in a private network (Class A).";
+                $response = ["status" => "success", "status_code" => 0, "response" => $msg];
+                if ($WebInterface)
+                    echo $msg;
                 $found = true;
                 break;
             } else if (($ipAddr > ip2long($localRanges[2]) && $ipAddr <= ip2long($localRanges[3]))) {
-                echo "This is a local IP. it only exists in a private network (Class B).";
+                $msg = "This is a local IP. it only exists in a private network (Class B).";
+                $response = ["status" => "success", "status_code" => 0, "response" => $msg];
+                if ($WebInterface)
+                    echo $msg;
                 $found = true;
                 break;
             } else if (($ipAddr > ip2long($localRanges[4]) && $ipAddr <= ip2long($localRanges[5]))) {
-                echo "This is a local IP. it only exists in a private network (Class C).";
+                $msg = "This is a local IP. it only exists in a private network (Class C).";
+                $response = ["status" => "success", "status_code" => 0, "response" => $msg];
+                if ($WebInterface)
+                    echo $msg;
                 $found = true;
                 break;
             } else if ($ipAddr > ip2long($carrierGradeRanges[0]) && $ipAddr <= ip2long($carrierGradeRanges[1])) {
-                echo "This is a Carrier Grade NAT Address.";
+                $msg = "This is a Carrier Grade NAT Address.";
+                $response = ["status" => "success", "status_code" => 0, "response" => $msg];
+                if ($WebInterface)
+                    echo $msg;
                 $found = true;
                 break;
             } else if ($ipAddr >= ip2long($d[3]) && $ipAddr <= ip2long($d[4])) {
-                echo "[" . strtoupper($d[0]) . "] ==> " . $d[1];
+                $resp['country_code'] = strtoupper($d[0]);
+                $resp['country_name'] = $d[1];
+                $response = ["status" => "success", "status_code" => 0, "response" => $resp];
+                if ($WebInterface)
+                    echo "[" . $resp['country_code'] . "] ==> " . $resp['country_name'];
                 $found = true;
                 break;
             }
         }
     }
-    if (!$found)
-        echo "Can't find country for this IP.";
+    if (!$found) {
+        $msg = "Can't find country for this IP.";
+        $response = ["status" => "error", "status_code" => 1, "response" => $msg];
+        if ($WebInterface)
+            echo $msg;
+    }
+} else {
+    $msg = "IP Address not supplied.";
+    $response = ["status" => "error", "status_code" => 1, "response" => $msg];
+    if ($WebInterface)
+        echo $msg;
+}
+
+if (!$WebInterface) {
+    $response['IP Address'] = $_REQUEST['ip'] ?? "";
+    echo json_encode($response);
 }
